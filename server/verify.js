8cd18efb53836bc15961dfb6530ba1ff6e1e22f4
Yash <72552910+kumaryash90@users.noreply.github.com>
2022-01-23 16:26:58 +0530

    Update verify.js

--- server/verify.js
+++ server/verify.js
@@ -3,15 +3,12 @@ const SHA256 = require('crypto-js/sha256');
 
 function verify(publicX, publicY, txnString, signature) {
     const ec = new EC('secp256k1');
-
-    // TODO: fill in the public key points
     const publicKey = {
         x: publicX,
         y: publicY
     }
     console.log("public key: ", publicKey);
     const key = ec.keyFromPublic(publicKey, 'hex');
-    //console.log("key: ", key);
     const txnHash = SHA256(txnString).toString();
     const sign = {
         r: signature.r,
